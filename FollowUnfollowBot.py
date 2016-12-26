48e8def5dbb4d89bb70514fd1092ecbe16250368
yomo710 <yomo710@gmail.com>
2016-12-25 21:48:26 -0500

    New: Send DM feature
    New features include sending DMs to followers, automatically.
    
    Other changes include making a list of nonmutuals.

--- FollowUnfollowBot.py
+++ FollowUnfollowBot.py
@@ -1,14 +1,17 @@
 # a bot that either follows or unfollows users
 # author - yousefissa
 
+# NOTE: You only have to change 1 thing: Change the user info below (screen name ,consumer_key, etc.)
+# If you want to harcode your message in, put it under senDM()
 # Know your limits with this bot. Look at the twitter API limits so you don't get locked out.
+
+
+
 
 import tweepy, time
 
 
-
-
-# ONLY THING TO CHANGE
+# Change this!
 # user info, change as necessary. Get this info from the twitter app page.
 screen_name = ''
 CONSUMER_KEY = ''
@@ -32,7 +35,8 @@ def mainMenu():
           '3. Follow users based on a keyword. \n'
           '4. Unfollow users that don\'t follow you back. \n'
           '5. Unfollow all users. \n'
-          '6. Get follower and following count. \n'
+          '6. Send a DM to users that follow you. \n'
+          '7. Get follower and following count. \n'
           )
     userChoice = input('Enter the number of the action that you want to take: ')
 
@@ -44,7 +48,8 @@ def mainMenu():
     3: followKeyword,
     4: unfollowBack,
     5: unfollowAll,
-    6: getCount
+    6: sendDM,
+    7: getCount 
     }
 
     # tries running the function according to the number. restarts if given a non-number or number not in range.
@@ -75,7 +80,7 @@ def followBack():
      # Makes a list of  those you don't follow back.
     nonFollowing = set(followers) - set(following)
 
-    print('Starting to follow.')
+    print('Starting to follow users...')
 
     # starts following users.
     for f in nonFollowing:
@@ -115,7 +120,7 @@ def followAll():
     theirFollowersReduced = set(theirFollowers) - set(following)
     # loops through theirFollowers and followers and adds non-mutual relationships to theirFollowersReduced
 
-    print('Starting to follow.')
+    print('Starting to follow users...')
     # loops through the list and follows users.
     for f in theirFollowersReduced:
         try:
@@ -152,10 +157,12 @@ def unfollowBack():
     # gets followers, following, total_followed
     followers, following, total_followed = getFriends()
 
-    print('Starting to unfollow.')
-    # loops through followers and following lists.
-    for f in following:
-        if f not in followers:
+    print('Starting to unfollow users...')
+
+    # makes a new list of users who don't follow you back.
+    nonMutuals = set(following) - set(followers)
+    for f in nonMutuals:
+        try:
             # unfollows non follower.
             api.destroy_friendship(f)
 
@@ -168,6 +175,10 @@ def unfollowBack():
             # print sleeping, sleep.
             print('Unfollowed user. Sleeping 2 seconds.')
             time.sleep(2)
+        except:
+            print('Could not follow users. Trying next user.')
+
+
     # prints the total followed, then continues
     print(total_followed)
     getCount()
@@ -197,6 +208,41 @@ def unfollowAll():
     Continue()
 
 
+# TODO: allow the importing of text lists so messages don't get flagged as spam.
+# Send a DM to users that follow you.
+def sendDM():
+    # gets followers, following, total_followed
+    followers, following, total_followed = getFriends()
+
+    print('A message will be sent')
+
+    # If you want to hardcode your message, remove the input from the message variable.
+    # gets the message from the user. 
+    message = input('Enter the message you want to send: \n')
+
+    # tries sending a message to your followers. 
+    print('Starting to send messages... ')
+    for f in followers:
+        try: 
+            # sends dm. 
+            api.send_direct_message(user_id=f, text=message)
+
+            # increment total_followed by 1
+            total_followed += 1
+            # print total unfollowed every 10
+            if total_followed % 10 == 0:
+                print(str(total_followed) + ' messages sent so far.')
+
+            print('Sent the user a DM. Sleeping 15 seconds.')
+            time.sleep(15)
+
+        except:
+            print('Could not send a DM. Trying another user.')
+    print(total_followed)
+    getCount()
+    Continue()
+
+
 
 # function to get follower/following count
 def getCount():
@@ -204,17 +250,16 @@ def getCount():
     followers, following, total_followed = getFriends()
     # prints the count.
     print('You follow {} users and {} users follow you.'.format(len(followers), len(followers)))
-    print('This is sometimes inaccurate due to the nature of the API and time. Be sure to double check. ')
+    print('This is sometimes inaccurate due to the nature of the API and updates. Be sure to double check. ')
     Continue()
 
 
 
 # function to continue
 def Continue():
-    # asks the user if they want to keep calculating.
+    # asks the user if they want to keep calculating, converts to lower case
     keepGoing = input('Do you want to keep going? Enter yes or no. \n'
-                      '')
-    keepGoing = keepGoing.lower()
+                      '').lower()
     # evaluates user's response.
     if keepGoing == 'yes':
         mainMenu()
